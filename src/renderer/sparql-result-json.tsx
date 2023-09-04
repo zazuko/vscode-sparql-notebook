fd4eccc1b81c77047aa186bbab135479c6774530
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2023-09-04 14:50:38 +0200

    switch renderer

--- src/renderer/sparql-result-json.tsx
+++ src/renderer/sparql-result-json.tsx
@@ -2,14 +2,15 @@ import { ActivationFunction, OutputItem } from 'vscode-notebook-renderer';
 import { SparqlResultJsonComponent } from './components/sparql-result-json-component/sparql-json-result-component';
 import { SparqlAskResultComponent } from './components/sparql-result-ask-component/sparql-ask-result-component';
 
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 export const activate: ActivationFunction = () => ({
   renderOutputItem(outputItem: OutputItem, element: HTMLElement) {
+    const root = createRoot(element);
     if (outputItem.json().hasOwnProperty("boolean")) {
-      render(<SparqlAskResultComponent sparqlAsResult={outputItem.json()} />, element);
+      root.render(<SparqlAskResultComponent sparqlAsResult={outputItem.json()} />);
     } else {
-      render(<SparqlResultJsonComponent sparqlResult={outputItem.json()} />, element);
+      root.render(<SparqlResultJsonComponent sparqlResult={outputItem.json()} />);
     }
   },
 });
