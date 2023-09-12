83111264d7985191f75fd33a6af3cddad3829e61
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2023-09-12 15:21:19 +0200

    cleanup bundle

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ const extensionConfig = {
   },
   resolve: {
     // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   },
   module: {
     rules: [
