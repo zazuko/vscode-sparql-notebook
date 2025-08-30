ac306386b7e036809763915cbeab957438f0661a
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2025-08-30 10:22:41 +0200

    before config manager

--- src/renderer/prefix.class.ts
+++ src/renderer/prefix.class.ts
@@ -3,22 +3,22 @@ import prefixes from '@zazuko/prefixes';
 import { PrefixMap } from './model/sparql-result-json.model';
 
 class Prefixes {
-    private _prefixes = prefixes;
-    private _loadedKeys: string[] = [];
+    #prefixes = prefixes;
+    #loadedKeys: string[] = [];
 
     private _add(prefix: string, uri: string) {
-        this._prefixes[prefix] = uri;
+        this.#prefixes[prefix] = uri;
     }
 
     loadMap(prefixes: PrefixMap) {
         const loadedKeys = Object.keys(prefixes);
 
         // delete all old keys
-        this._loadedKeys.forEach(key => {
-            delete this._prefixes[key];
+        this.#loadedKeys.forEach(key => {
+            delete this.#prefixes[key];
         });
 
-        this._loadedKeys = loadedKeys;
+        this.#loadedKeys = loadedKeys;
 
         Object.keys(prefixes).forEach(prefix => {
             this._add(prefix, prefixes[prefix]);
@@ -30,7 +30,7 @@ class Prefixes {
     }
 
     shrink(uri: string): string {
-        return shrink(uri, this._prefixes);
+        return shrink(uri, this.#prefixes);
     }
 
     expand(curie: string): string {
