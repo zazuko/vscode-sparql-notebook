56438f434e13e09dbafe2b9d7f69963251675bde
Hofstetter Benjamin (extern) <benjamin.hofstetter@mobi.ch>
2025-09-02 21:01:33 +0200

    remove path

--- src/extension/sparql-connection-menu/endpoint-connection-list-item.class.ts
+++ src/extension/sparql-connection-menu/endpoint-connection-list-item.class.ts
@@ -1,27 +1,34 @@
-import * as vscode from "vscode";
-import * as path from "path";
-import { EndpointConfiguration } from "../model/endpoint-configuration-v1";
+import {
+    TreeItem,
+    TreeItemCollapsibleState,
+    Command,
+    Uri
+} from "vscode";
 
+import { EndpointConfigurationV1 } from "../model/endpoint-configuration-v1";
 
-export class EndpointConnectionListItem extends vscode.TreeItem {
+export class EndpointConnectionListItem extends TreeItem {
     constructor(
-        public readonly config: EndpointConfiguration,
+        public readonly config: EndpointConfigurationV1,
         public readonly isActive: boolean,
-        public readonly collapsibleState: vscode.TreeItemCollapsibleState,
-        public readonly command?: vscode.Command
+        public override readonly collapsibleState: TreeItemCollapsibleState,
+        public override readonly command?: Command
     ) {
         super(config.name, collapsibleState);
 
+        function joinAssetPath(...parts: string[]): string {
+            return parts.join("/").replace(/\/+/g, "/");
+        }
         if (isActive) {
             this.iconPath = {
-                dark: vscode.Uri.file(path.join(assetsPath, "dark", "endpoint-connected.svg")),
-                light: vscode.Uri.file(path.join(assetsPath, "light", "endpoint-connected.svg")),
+                dark: Uri.file(joinAssetPath(assetsPath, "dark", "endpoint-connected.svg")),
+                light: Uri.file(joinAssetPath(assetsPath, "light", "endpoint-connected.svg")),
             };
             this.description = "Connected";
         } else {
             this.iconPath = {
-                dark: vscode.Uri.file(path.join(assetsPath, "dark", "endpoint.svg")),
-                light: vscode.Uri.file(path.join(assetsPath, "light", "endpoint.svg")),
+                dark: Uri.file(joinAssetPath(assetsPath, "dark", "endpoint.svg")),
+                light: Uri.file(joinAssetPath(assetsPath, "light", "endpoint.svg")),
             };
             this.description = "Inactive";
         }
@@ -29,4 +36,13 @@ export class EndpointConnectionListItem extends vscode.TreeItem {
     }
 }
 
-export const assetsPath = path.join(__filename, "..", "..", "assets");
+// __filename is an absolute path, so we can use string ops to get the assets folder
+function getAssetsPath(filename: string): string {
+    // Remove file name, go up two directories, then add /assets
+    const parts = filename.split("/");
+    parts.pop(); // remove file name
+    parts.pop(); // up one
+    parts.pop(); // up two
+    return parts.join("/") + "/assets";
+}
+export const assetsPath = getAssetsPath(__filename);
